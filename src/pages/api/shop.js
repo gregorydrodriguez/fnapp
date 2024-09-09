5875318a52d989617db92d0e30660b6b3da7af67
Gregory Rodriguez <grodriguez13@myseneca.ca>
2024-09-09 00:38:46 -0400

    remove .env

---
+++ src/pages/api/shop.js
@@ -0,0 +1,17 @@
+// pages/api/shop.js
+
+export default async function handler(req, res) {
+    const apiUrl = 'https://fortnite-api.com/v2/shop/br';
+
+    try {
+        const response = await fetch(apiUrl);
+        if (!response.ok) {
+            return res.status(response.status).json({ error: 'Error fetching shop data' });
+        }
+
+        const data = await response.json();
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(500).json({ error: 'Server Error' });
+    }
+}
--- src/pages/shop.js
+++ src/pages/shop.js
@@ -1,24 +1,26 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import { Pagination } from 'react-bootstrap';
 import styles from '../styles/Home.module.css';
+// unused to continue developing
 import ProtectedRoute from '../../components/ProtectedRoute';
 
 export default function Shop() {
     const [items, setItems] = useState([]);
-    const [cart, setCart] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const itemsPerPage = 8;
 
     useEffect(() => {
         const fetchItems = async () => {
             try {
-                const response = await axios.get('https://fortnite-api.com/v2/shop/br');
+                const response = await fetch('https://fortnite-api.com/v2/shop/br');
+                const data = await response.json();
                 const uniqueItems = new Map();
-                response.data.data.featured.entries.forEach(item => {
+                data.data.featured.entries.forEach(item => {
                     if (!uniqueItems.has(item.items[0].name)) {
                         uniqueItems.set(item.items[0].name, item);
                     }
                 });
-                // Fetches 18 unique items
-                setItems(Array.from(uniqueItems.values()).slice(0, 18));
+                setItems(Array.from(uniqueItems.values()));
             } catch (error) {
                 console.error('Error fetching items:', error);
             }
@@ -27,31 +29,36 @@ export default function Shop() {
         fetchItems();
     }, []);
 
-    const addToCart = (item) => {
-        setCart([...cart, item]);
-        alert(`${item.items[0].name} added to cart!`);
-    };
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+    const currentItems = items.slice(indexOfFirstItem, indexOfLastItem);
+
+    const handlePageChange = (pageNumber) => setCurrentPage(pageNumber);
 
     return (
-        <ProtectedRoute>
             <div className={styles['shop-container']}>
                 <h2 className={styles['section-title']}>PlayerShop</h2>
                 <div className={styles['items-grid']}>
-                    {items.map((item, index) => (
+                    {currentItems.map((item, index) => (
                         <div key={index} className={styles['item-card']}>
                             <img src={item.items[0].images.icon} alt={item.items[0].name} className={styles['item-image']} />
                             <h3 className={styles['item-title']}>{item.items[0].name}</h3>
                             <p className={styles['item-description']}>{item.items[0].description}</p>
                             <p className={styles['item-price']}>Cost: {item.regularPrice} V-Bucks</p>
-                            <button
-                                className={styles['cart-button']}
-                                onClick={() => addToCart(item)}>
-                                Add to Cart
-                            </button>
                         </div>
                     ))}
                 </div>
+                <Pagination className="justify-content-center mt-3">
+                    {[...Array(Math.ceil(items.length / itemsPerPage)).keys()].map(pageNumber => (
+                        <Pagination.Item
+                            key={pageNumber + 1}
+                            active={currentPage === pageNumber + 1}
+                            onClick={() => handlePageChange(pageNumber + 1)}
+                        >
+                            {pageNumber + 1}
+                        </Pagination.Item>
+                    ))}
+                </Pagination>
             </div>
-        </ProtectedRoute>
     );
 }
